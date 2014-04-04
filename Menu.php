4949050214b2b1a8392b486d2e95960fff580926
William <William@172.16.233.125>
2014-04-04 12:31:41 +0200

    Menu

---
+++ Menu.php
@@ -0,0 +1,22 @@
+<html>
+    <meta charset="utf-8" />
+        <title>MusicBox</title>
+		<link rel="stylesheet" href="accueil.css" />
+                
+                <div class="menuhori">
+		
+
+<ul id="menu_horizontal">
+    <li><a href="Artiste.php">Artiste</a></li>
+    <li><a href="Salles.php">Salles</a></li>
+<li><a href="livreor.php">Agenda</a></li>
+<li><a href="contact.php">Forum</a></li>
+<li><a href="admin.php">Ma box</a></li>
+</ul>
+
+<h8>o O   Offline</h8>
+
+</div>
+</html>
+
+
