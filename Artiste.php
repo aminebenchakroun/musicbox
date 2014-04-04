a05a4c5085e1506b7cd21f2d46d7a11da57c1c5c
William <William@172.16.233.125>
2014-04-04 12:29:46 +0200

    Page Artiste 1er version

---
+++ Artiste.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+        <meta charset="utf-8" />
+        <title>MusicBox</title>
+		<link rel="stylesheet" href="accueil.css" />
+    </head>
+    <body>
+ 
+        <?php include("Header.php"); ?>
+ 
+        <?php include("Menu.php"); ?>
+        
+</body>
+</html>
+
