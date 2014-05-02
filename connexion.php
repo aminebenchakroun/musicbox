ba0fdeb8929dee324cdac01c02f2afa59774070e
apeschel1 <alexandrepeschel@hotmail.fr>
2014-05-02 13:16:17 +0200

    nouvelle page inscription

--- connexion.php
+++ connexion.php
@@ -7,27 +7,24 @@
 	<link rel="stylesheet" href="accueil.css" />
 </head>
 
-<body>
+<header>
+<div id = "Inscription">
+<img src = "" alt = "Logo Musicbox" id = "logo"/>
+</div>
+</header>
 
+<body>
 <h1>Connectez vous à votre MusicBox</h1>
 
-<form method="post" action="connexion.php">
-	
 	<fieldset class="Connexion">
-		<legend><b><h2>Inscription</h2></b></legend>
-		<br/>
+		<legend><b><h2>Inscription</h2></b></legend><br/>
 		
-	
 		<label for="pseudo"><b>Identifiants:</b></label>
 		<input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" size="30" maxlength="20" required/>
 		<input type="password" name="motdepasse" id="motdepasse" placeholder="Mot de passe" size="30" maxlength="20" required/>
-		<input type="password" name="confirmationmotdepasse" id="confirmationmotdepasse" placeholder="Confirmation du mot de passe" size="30" required/><br/><br/>
-	
 
-		<input type="submit" value="Envoyer" /><br/>
+		<input type="submit" name="submit" value="Envoyer" /><br/>
 	</fieldset>
 	
-	
-</form>
 </body>
 </html>
